21613d835b54e12ef17386e9334f2eb89a6d8052
mattperkins <github@mattperkins.com>
2018-04-15 02:21:01 +0100

    style game component

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-import {
-  StyleSheet,
-} from 'react-native'
 
 import Game from './Game'
 
 export default class App extends Component {
   render() {
     return (
-      <Game />
+      <Game randomNumCount={4} />
     )
   }
 }
--- Game.js
+++ Game.js
@@ -1,27 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 import {
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+    static propTypes = {
+        randomNumCount: PropTypes.number.isRequired,
+    }
+    
+    randomNums = Array
+    .from({ length: this.props.randomNumCount })
+    .map(() => 1 + Math.floor(10 * Math.random()))
+    
+    target = this.randomNums
+        .slice(0, this.props.randomNumCount -2)
+        .reduce((acc, curr) => acc + curr, 0)
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-            Start
+        
+        <Text style={styles.target}>
+            {this.target}
         </Text>
+
+        <View style={styles.randomContainer}>
+            {this.randomNums.map((randomNum, index) => 
+                <View style={styles.circle} key={index}>
+                    <Text style={styles.randomText}>{randomNum}</Text>
+                </View>
+        )}
+        </View>
+
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: 'orange',
   },
-});
+  target: {
+      fontSize: 40,
+      backgroundColor: '#aaa',
+      margin: 50,
+      textAlign: 'center'
+  },
+  randomContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+  },
+  circle: {
+      borderRadius: 50,
+      width: 100,
+      margin: 20,
+      backgroundColor: '#999',
+  },
+  randomText: {
+      fontSize: 35,
+      textAlign: 'center',
+  },
+})
